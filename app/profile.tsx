744a086e8f72808f4287bbc13a906d7c18cfa0bb
Jyundev <jyunxxxxx@google.com>
2024-08-17 08:50:19 +0900

    login 추가

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,15 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import ColorList from '../components/ColorList'
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Profile = () => {
+
+
+  const router = useRouter();
+
+  const login = () => {
+    router.push('/login')
+  }
   return (
-    <View>
-       <ColorList color="#ff0000" />
-       </View>
-  )
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={login}
+      >
+        <Text style={styles.buttonText}>로그인</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  button: {
+    backgroundColor: 'white', 
+    borderColor: 'black',
+    height: 50,
+    borderRadius: 30,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '40%', 
+  },
+  buttonText: {
+    color: 'black', 
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+})
